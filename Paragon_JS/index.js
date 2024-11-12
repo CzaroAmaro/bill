16993324aff9608479e861cc28f0b10f329bb130
Cezary Babik <babik.czarek@gmail.com>
2024-11-12 19:16:37 +0100

    test

--- Paragon_JS/index.js
+++ Paragon_JS/index.js
@@ -1,7 +1,8 @@
 
 let produkty = [
     {nazwa: "Masło", ilość: 1, cena: 8},
-    {nazwa: "Jajka", ilość: 1, cena: 10}
+    {nazwa: "Jajka", ilość: 1, cena: 10},
+    {nazwa: "Woda", ilość: 1, cena: 3}
 ]
 
 
